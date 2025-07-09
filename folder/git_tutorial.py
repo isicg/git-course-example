7322f996b1cd20ff67b9519fba1e4684a2261eae
Goran Isic <isicg@protonmail.com>
2025-07-09 18:19:45 +0200

    resolving merge conflict with docstring and performant add_numbers()

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -1,6 +1,6 @@
 def add_number(a, b, c):
     """Return the sum of a, b and c."""
-    return sum([a, b, c])
+    return sum([a, b, c]) # merge conflict resolved
 
 # 1. extend add_numbers to allow an arbitrary number of parameters
 # 2. investigate and resolve security exploit of add_numbers()
